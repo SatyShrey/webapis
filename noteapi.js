e811a7f82158c3479009d39d167dc7c66b008b29
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2024-09-23 05:55:04 +0530

    Add files via upload

--- noteapi.js
+++ noteapi.js
@@ -1,22 +1,14 @@
 
 require('dotenv').config()
 
-var cors= require('cors');
-const app=express()
-const mongoose=require('mongoose')
-const mongoClient=require('mongodb').MongoClient;
-app.use(express.json())
-app.use(cors());
 const express=require('express')
+const app=express()
+app.use(express.json())
 
+const cors= require('cors');
+app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://webapis-j8hu.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-  
+const mongoClient=require('mongodb').MongoClient;
 
 //initialize database............................
 let database='Database'
