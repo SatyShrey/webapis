a15431b8f01b35caf6860ef67a44417c9148c818
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2024-09-23 08:05:21 +0530

    Add files via upload

--- noteapi.js
+++ noteapi.js
@@ -8,31 +8,30 @@ app.use(express.json())
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const mongoUrl = process.env.mongoUrl;
 const PORT = process.env.PORT;
 const mongoClient = require('mongodb').MongoClient;
 
+//database connections.................
+let database = 'database'
+mongoClient.connect(mongoUrl).then(clientObject => {
+    database = clientObject.db('My-notes');
+})
 
 //default response.................................
 app.get('/', (req, res) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('My-notes');
-        res.send('Welcome to Note API.')
-    })
+    res.send('Welcome to Note API.')
 })
 
 
 //all notes....................................
 app.get('/notes', (request, response) => {
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('My-notes');
-        database.collection('notes').find({}).toArray().then(documents => {
-            response.send(documents);
-            response.end();
-        })
+    database.collection('notes').find({}).toArray().then(documents => {
+        response.send(documents);
+        response.end();
     })
 })
 
@@ -43,22 +42,16 @@ app.post('/addnote', (request, response) => {
         Descriptions: request.body.Descriptions,
         uid: Date.now().toString()
     }
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('My-notes');
-        database.collection('notes').insertOne(newNote);
-        response.end();
-    })
+    database.collection('notes').insertOne(newNote);
+    response.end();
 })
 
 
 //delete notes.............................................
 app.delete('/deletenote/:id', (request, response) => {
     var id = request.params.id;
-    mongoClient.connect(mongoUrl).then(clientObject => {
-        var database = clientObject.db('My-notes');
-        database.collection('notes').deleteOne({ uid: id });
-        response.end();
-    })
+    database.collection('notes').deleteOne({ uid: id });
+    response.end();
 })
 
 
